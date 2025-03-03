3d8100f0c6aa249da88d23de8b22be2227c1f4f7
yussuf <yusufmim2001@gmail.com>
2025-03-03 20:05:12 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution in this file!
+const companyName = "Scuber"; // Must be a constant
+let mostProfitableNeighborhood = "Chelsea"; // Use let because it might change
+let companyCeo = "Susan Smith"; // Use let as it's not a constant
